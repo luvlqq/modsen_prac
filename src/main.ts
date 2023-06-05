26f03909b67e3068a08766bdd2f0347696a08a6f
luvlqq <lualgod92@gmail.com>
2023-06-05 15:04:30 +0300

    change absolute path to relative path

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+if (!process.env.IS_TS_NODE) {
+  require('module-alias/register');
+}
+
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
--- src/meetups/meetups.service.ts
+++ src/meetups/meetups.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@app/prisma/prisma.service';
 
 @Injectable()
-export class MeetupsService {}
+export class MeetupsService {
+  constructor(private readonly prisma: PrismaService) {}
+}
