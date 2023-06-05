3f7f28bd2a6abb21952946663789d6c9fc35a161
luvlqq <lualgod92@gmail.com>
2023-06-05 14:32:15 +0300

    add swagger to project. create basic tags and descriptions for routes

--- src/meetups/meetups.controller.ts
+++ src/meetups/meetups.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { MeetupsService } from './meetups.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Meetups')
 @Controller('meetups')
 export class MeetupsController {
   constructor(private readonly meetupsService: MeetupsService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Get all meetups' })
+  getAllMeetups() {}
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get meetup by id' })
+  getMeetupById() {}
+
+  @Post('createmeetup')
+  @ApiOperation({ summary: 'Create a meetup' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Success' })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad Request' })
+  createAMeetup() {}
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Change meetup parameters by id' })
+  changeInfoInMeetup() {}
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete meetup by id' })
+  deleteMeetupById() {}
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get user meetup' })
+  getUserMeetups() {}
 }
