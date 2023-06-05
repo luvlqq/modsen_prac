f12ac09d0333966e0e39fede35e7a7fb558ffdac
luvlqq <lualgod92@gmail.com>
2023-06-05 15:03:53 +0300

    init db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MeetupsModule } from './meetups/meetups.module';
 import { UsersModule } from './users/users.module';
+import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, MeetupsModule, UsersModule],
+  imports: [AuthModule, MeetupsModule, UsersModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
