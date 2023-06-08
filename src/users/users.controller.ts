15fc0b79dd2a0592b4df016994e879251eff5593
luvlqq <lualgod92@gmail.com>
2023-06-08 22:19:04 +0300

    Show user info by access token

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { GetCurrentUserId } from '@app/src/common/decorators/get.current.userId.decorator';
 
 @ApiTags('Users')
 @Controller('users')
@@ -10,4 +11,16 @@ export class UsersController {
   @Get(':id')
   @ApiOperation({ summary: 'Get user meetup' })
   getUserMeetups() {}
+
+  @Get()
+  @ApiOperation({ summary: 'Get user info by token' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Success' })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad Request' })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Unauthorized',
+  })
+  getUserInfo(@GetCurrentUserId() userId: number) {
+    return this.usersService.getUserInfo(userId);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,4 +4,8 @@ import { PrismaService } from '@app/prisma/prisma.service';
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
+
+  getUserInfo(id: number) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
 }
