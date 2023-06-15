a2a0d25e27809f9e0db4e2e3536f8eed8bc2b6f3
luvlqq <lualgod92@gmail.com>
2023-06-15 22:32:44 +0300

    cookie parser install and add to main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as process from 'process';
+
 if (!process.env.IS_TS_NODE) {
   require('module-alias/register');
 }
@@ -6,21 +8,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
-import config from '@app/src/config/config';
+import * as cookieParser from 'cookie-parser';
+import config from './config/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
+
   const swagger = new DocumentBuilder()
     .setTitle('Meetup API')
     .setDescription('The meetup API description')
     .setVersion('1.0')
     .build();
-
   const document = SwaggerModule.createDocument(app, swagger);
   SwaggerModule.setup('doc', app, document);
-
   await app.listen(config.application.port);
 }
 bootstrap();
